bac8065af7ac7bc74a7c344fee4607c07a96e59b
sohaihub <soha.r.ihub@snsgroups.com>
2025-05-13 15:11:46 +0530

    Update RequestDemo.tsx

--- src/pages/RequestDemo.tsx
+++ src/pages/RequestDemo.tsx
@@ -37,11 +37,7 @@ const RequestDemo: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col bg-[#0A0A29]">
       {/* Background elements */}
-      <div className="absolute top-20 left-20 w-1 h-1 bg-white rounded-full"></div>
-      <div className="absolute top-40 right-40 w-2 h-2 bg-white rounded-full"></div>
-      <div className="absolute bottom-60 left-1/4 w-1.5 h-1.5 bg-white rounded-full"></div>
-      <div className="absolute top-96 right-1/3 w-1 h-1 bg-white rounded-full"></div>
-      <div className="absolute top-1/2 left-10 w-2 h-2 bg-white rounded-full"></div>
+
       
       {/* Background circular gradients */}
       <div className="absolute -top-20 -right-20 w-80 h-80 rounded-full bg-[#E2FF55]/20 blur-3xl"></div>
