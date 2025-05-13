b24795002e3e5c667b5a29e5515eeb28895bdc88
sohaihub <soha.r.ihub@snsgroups.com>
2025-05-13 15:25:02 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,11 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Solutions from './pages/Solutions';
 import AIProducts from './pages/AIProducts';
-import Enterprise from './pages/Enterprise';
-import Blog from './pages/Blog';
-import CaseStudies from './pages/CaseStudies';
-import Guides from './pages/Guides';
-import About from './pages/About';
-import Careers from './pages/Careers';
 import Contact from './pages/Contact';
-import Terms from './pages/Terms';
-import Privacy from './pages/Privacy';
-import Security from './pages/Security';
-import ComingSoon from './pages/ComingSoon'; // Placeholder for unimplemented pages
+import ComingSoon from './pages/ComingSoon';
+import Resources from './pages/Resources';
+import RequestDemo from './pages/RequestDemo';
+import Login from './pages/Login';
 
 const App: React.FC = () => {
   return (
@@ -22,18 +16,19 @@ const App: React.FC = () => {
         <Route path="/" exact component={Home} />
         <Route path="/solutions" component={Solutions} />
         <Route path="/ai-products" component={AIProducts} />
-        <Route path="/pricing" component={Pricing} />
-        <Route path="/enterprise" component={ComingSoon} />
-        <Route path="/resources/library" component={ResourceLibrary} />
-        <Route path="/resources/blog" component={Blog} />
-        <Route path="/resources/case-studies" component={CaseStudies} />
-        <Route path="/resources/guides" component={Guides} />
-        <Route path="/about" component={About} />
-        <Route path="/careers" component={Careers} />
+        <Route path="/resources" component={Resources} />
+        <Route path="/request-demo" component={RequestDemo} />
+        <Route path="/login" component={Login} />
         <Route path="/contact" component={Contact} />
-        <Route path="/terms" component={Terms} />
-        <Route path="/privacy" component={Privacy} />
-        <Route path="/security" component={Security} />
+        {/* Placeholder routes */}
+        <Route path="/pricing" component={ComingSoon} />
+        <Route path="/enterprise" component={ComingSoon} />
+        <Route path="/about" component={ComingSoon} />
+        <Route path="/careers" component={ComingSoon} />
+        <Route path="/terms" component={ComingSoon} />
+        <Route path="/privacy" component={ComingSoon} />
+        <Route path="/security" component={ComingSoon} />
+        {/* Catch-all */}
         <Route path="*" component={ComingSoon} />
       </Switch>
     </Router>
