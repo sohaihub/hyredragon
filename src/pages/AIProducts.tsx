0669e62311c4d407f400e653b803b24e0fbfb7f7
sohaihub <soha.r.ihub@snsgroups.com>
2025-05-13 15:09:55 +0530

    Update AIProducts.tsx

--- src/pages/AIProducts.tsx
+++ src/pages/AIProducts.tsx
@@ -146,18 +146,7 @@ const AIProducts: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col bg-[#0A0A29]">
       {/* Background elements */}
-      <div className="fixed top-0 left-0 w-full h-full">
-        <div className="absolute top-20 left-20 w-1 h-1 bg-white rounded-full"></div>
-        <div className="absolute top-40 right-40 w-2 h-2 bg-white rounded-full"></div>
-        <div className="absolute bottom-60 left-1/4 w-1.5 h-1.5 bg-white rounded-full"></div>
-        <div className="absolute top-96 right-1/3 w-1 h-1 bg-white rounded-full"></div>
-        
-        {/* Background circular gradients */}
-        <div className="absolute -top-20 -right-20 w-96 h-96 rounded-full bg-[#E2FF55]/10 blur-3xl"></div>
-        <div className="absolute top-1/2 left-1/4 w-80 h-80 rounded-full bg-[#7B78FF]/10 blur-3xl"></div>
-        <div className="absolute bottom-1/3 -left-40 w-96 h-96 rounded-full bg-[#E2FF55]/5 blur-3xl"></div>
-      </div>
-      
+
       <Header />
       
       <main className="flex-grow relative z-10">
